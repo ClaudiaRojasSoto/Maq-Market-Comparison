4c91007f8f9496da71221eb3ce28a5c83d8f6ebe
Claudia  P. R. Soto <97201255+ClaudiaRojasSoto@users.noreply.github.com>
2023-12-14 10:56:01 -0300

    Change the language of the error text and the commits language to english instead spanish

--- allProducts.js
+++ allProducts.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-// Función para raspar los productos de una subcategoría específica
+// Function to have the products on a specific category
 const scrapeProductsFromSubcategory = async (subcategory) => {
   try {
     const { data } = await axios.get(subcategory.href);
@@ -25,12 +25,12 @@ const scrapeProductsFromSubcategory = async (subcategory) => {
 
     return products;
   } catch (error) {
-    console.error(`Hubo un error al raspar la subcategoría: ${subcategory.title}`, error);
+    console.error(`There was an error when scraping the category: ${subcategory.title}`, error);
     return [];
   }
 };
 
-// Función para raspar las subcategorías de la URL principal
+// Function to scrape subcategories from the main url
 const scrapeSubcategories = async (url) => {
   try {
     const { data } = await axios.get(url);
@@ -44,44 +44,44 @@ const scrapeSubcategories = async (url) => {
 
     return subcategories;
   } catch (error) {
-    console.error('Hubo un error al raspar las subcategorías:', error);
+    console.error('There was an error when scraping the subcategories:', error);
     return [];
   }
 };
 
-// Función para guardar los datos en un archivo JS
+// Function to save data to a js file
 const saveDataToJsFile = (data, fileName) => {
   const jsContent = `module.exports = ${JSON.stringify(data, null, 2)};`;
   fs.writeFile(fileName, jsContent, 'utf8', (err) => {
     if (err) {
-      console.error(`Ocurrió un error al escribir en el archivo ${fileName}:`, err);
+      console.error(`An error occurred while writing to the file ${fileName}:`, err);
     } else {
-      console.log(`Los datos han sido guardados en ${fileName}`);
+      console.log(`The data has been saved in ${fileName}`);
     }
   });
 };
 
-// Función de pausa (sleep)
+// Pause function (sleep)
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-// Función para automatizar el scraping de todas las subcategorías y productos
+// Feature to automate scraping of all subcategories and products
 const scrapeAndSaveAllProducts = async (url) => {
   try {
     const subcategories = await scrapeSubcategories(url);
 
-    // Uso de map y Promise.all para manejar las promesas en paralelo
+    // Using map and Promise.all to handle promises in parallel
     const productsArrays = await Promise.all(subcategories.map(async (subcategory) => {
-      await sleep(1000); // Agrega un retraso para evitar sobrecargar el servidor
+      await sleep(1000); // Delay to avoid overloading the server
       return scrapeProductsFromSubcategory(subcategory);
     }));
 
-    const allProducts = productsArrays.flat(); // Aplana el array de arrays
+    const allProducts = productsArrays.flat(); // Flatten the array of arrays to combine
     saveDataToJsFile(allProducts, 'allProductsData.js');
   } catch (error) {
-    console.error('Hubo un error al procesar todos los productos:', error);
+    console.error('There was an error processing all products:', error);
   }
 };
 
-// URL de la página principal de subcategorías
+// Subcategory Home Page URL
 const url = 'https://www.soonparts.com/departments';
 scrapeAndSaveAllProducts(url);
